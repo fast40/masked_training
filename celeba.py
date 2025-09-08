52e386864083b303bf9fc33e3d1595e9f8139e3f
fast40 <eclfast@gmail.com>
2025-09-08 11:34:22 -0700

    Some working stuff

--- celeba.py
+++ celeba.py
@@ -1,25 +1,24 @@
 import pathlib
 
+import torch
 from torch.utils.data import Dataset, DataLoader
 from torchvision.transforms import Compose, CenterCrop, ToTensor
 from PIL import Image
 
 
 class ImageDataset(Dataset):
-    def __init__(self, root_dir, transform=Compose([CenterCrop((128, 128)), ToTensor()])):
+    def __init__(self, root_dir, transform=None):
         self.image_paths = list(pathlib.Path(root_dir).glob('*.jpg'))
-        self.transform = transform
+        self.transform = transform if transform is not None else Compose([CenterCrop((128, 128)), ToTensor()])
 
     def __len__(self):
         return len(self.image_paths)
 
-    def __getitem__(self, idx):
+    def __getitem__(self, idx) -> torch.Tensor:
         image = Image.open(self.image_paths[idx]).convert('RGB')
 
-        if self.transform:
-            image = self.transform(image)
-
-        return image
+        return self.transform(image)
+    
 
 
 dataset = ImageDataset('celeba')
--- main.py
+++ main.py
@@ -4,46 +4,83 @@ The goal of this program is to evaluate the efficacy of adding individual neuron
 
 import torch
 from torch import nn
+from torch import optim
 from torch.nn import functional as F
 from torch.utils.data import DataLoader
+from torch.utils.tensorboard import SummaryWriter
 
 from celeba import ImageDataset
 
 torch.manual_seed(1337)
 
 
-BATCH_SIZE = 2
-INPUT_DIMS = 6
-HIDDEN_DIMS = 5
-
+BATCH_SIZE = 2**6
+INPUT_DIMS = 3 * 128 * 128
+HIDDEN_DIMS = 2**8
+DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+print(f'Using device {DEVICE}')
 
 class Model(nn.Module):
     def __init__(self):
         super().__init__()
 
-        self.layer1 = nn.Linear(INPUT_DIMS, HIDDEN_DIMS)
-        self.layer2 = nn.Linear(HIDDEN_DIMS, INPUT_DIMS)
+        self.encoder = nn.Sequential(
+            nn.Linear(INPUT_DIMS, HIDDEN_DIMS * 4),
+            nn.ReLU(),
+            nn.Linear(HIDDEN_DIMS * 4, HIDDEN_DIMS),
+            nn.ReLU(),
+        )
+
+        self.decoder = nn.Sequential(
+            nn.Linear(HIDDEN_DIMS, HIDDEN_DIMS * 4),
+            nn.ReLU(),
+            nn.Linear(HIDDEN_DIMS * 4, INPUT_DIMS),
+            nn.Sigmoid()
+        )
+
+        #
+        # self.layer2 = nn.Linear(HIDDEN_DIMS, INPUT_DIMS)
 
     def forward(self, x, calculate_loss=False):
-        layer1_output = self.layer1(x)
+        shape = x.shape
+        x = x.flatten(start_dim=1)
+        encoder_output = self.encoder(x)
 
         if calculate_loss:
-            masked_inputs = layer1_output.repeat(1, HIDDEN_DIMS).view(-1, HIDDEN_DIMS, HIDDEN_DIMS).tril().view(-1, HIDDEN_DIMS)
-            layer2_output = self.layer2(masked_inputs)
-            loss = F.mse_loss(layer2_output, x.repeat(1, HIDDEN_DIMS).view(-1, INPUT_DIMS))
+            masked_inputs = encoder_output.repeat(1, HIDDEN_DIMS).view(-1, HIDDEN_DIMS, HIDDEN_DIMS).tril().view(-1, HIDDEN_DIMS)
+            # decoder_output = self.decoder(encoder_output)
+            decoder_output = self.decoder(masked_inputs)
+            # loss = F.mse_loss(decoder_output, x)
+            loss = F.mse_loss(decoder_output, x.repeat(1, HIDDEN_DIMS).view(-1, INPUT_DIMS))
 
-            return layer2_output, loss
+            return decoder_output.view(-1, *shape[1:]), loss
         else:
-            layer2_output = self.layer2(layer1_output)
+            decoder_output = self.decoder(encoder_output)
 
-            return layer2_output
+            return decoder_output.view(-1, *shape[1:])
 
 
 m = Model()
+m.to(DEVICE)
+optimizer = optim.Adam(m.parameters(), lr=1e-3)
+
+writer = SummaryWriter('runs/exp4')
 
 dataset = ImageDataset('celeba')
-dataloader = DataLoader(dataset, batch_size=1, shuffle=False)
+dataloader = DataLoader(dataset, batch_size=BATCH_SIZE, shuffle=False)
 
+i = 0
 while True:
     for batch in dataloader:
-        output, loss =  m(batch, batch)
+        batch = batch.to(DEVICE)
+        optimizer.zero_grad()
+        output, loss =  m(batch, calculate_loss=True)
+        loss.backward()
+        optimizer.step()
+
+        writer.add_scalar('training_loss', loss.item(), i)
+        image = torch.stack([dataset[i] for i in range(5)])
+        writer.add_images('image_input', image, i)
+        writer.add_images('image_ouptut', m(image.to(DEVICE)).detach().cpu(), i)
+        print(loss.item())
+        i += 1
